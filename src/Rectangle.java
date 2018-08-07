1e6d2ffb93653bb36bd94fd9587e6511195ac425
mike <mike@jenkins.com>
2018-08-08 01:45:20 +0200

    cambios build.xml 9 agosto 2

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,18 +1,17 @@
-    public class Rectangle {
-      public int lenght;
-      public int width;
-      
+public class Rectangle {
+  public int length;
+  public int width;
 
-      public Rectangle(int length, int width)  {
-        this.length = lenght;
-        this.width = width;
-      }
+  public Rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
+  }
 
-      public int getArea() {
-        return lenght * width;
-      }
+  public int getArea() {
+    return length * width;
+  }
 
-      public int get Perimeter() {
-        return 2 * (length + width);
-      }
-    }
+  public int getPerimeter() {
+    return 2 * (length + width);
+  }
+}
